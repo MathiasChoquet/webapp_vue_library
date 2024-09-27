73469142be1e57ebc1b790c16aeb18d911b03d17
Mathias CHOQUET <mathias.choquet@gmail.com>
2024-09-27 10:12:56 +0200

    add routing

---
+++ src/views/LibraryView.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Author from '../components/Author.vue';
+import Book from '../components/Book.vue';
+import HelloWorld from '../components/HelloWorld.vue';
+</script>
+
+<template>
+  <Book class="generic-component"/>
+  <main>
+    <Author class="generic-component"/>
+  </main>
+  <div class="generic-component">
+    <HelloWorld />
+  </div>
+</template>
